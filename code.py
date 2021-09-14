d1fc5fd35866cb70ddae0ee9b16f5004c56806e5
BjoernSchilberg <BjoernSchilberg@users.noreply.github.com>
2021-09-14 22:30:28 +0200

    Test github dev editor

--- code.py
+++ code.py
@@ -9,6 +9,7 @@ for i in range(50):
     painter.left(123) # Let's go counterclockwise this time 
     
 painter.pencolor("red")
+
 for i in range(50):
     painter.forward(100)
     painter.left(123)
